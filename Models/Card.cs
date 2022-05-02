e728599d31e50ed8af65c373c6d0c944288f0131
abaporu-C <77457350+abaporu-C@users.noreply.github.com>
2022-05-01 20:28:16 -0500

    Index Page

---
+++ Models/Card.cs
@@ -0,0 +1,9 @@
+﻿namespace EDH2COD.Models
+{
+    public class Card
+    {
+        public string? Name { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
---
+++ Models/CodFile.cs
@@ -0,0 +1,11 @@
+﻿namespace EDH2COD.Models
+{
+    public class CodFile
+    {        
+        public string? DeckName { get; set; }
+
+        public List<Card>? Main { get; set; }
+
+        public List<Card>? Side { get; set; }
+    }
+}
